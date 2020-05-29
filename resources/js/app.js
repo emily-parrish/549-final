6af6199302290b83838d72b25e0dbe7b3383fa5e
emily <eparrish@uw.edu>
2020-05-29 13:44:31 -0700

    Fiddled with javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,45 @@
 // something kinda like this but not
 //needs to look at dropdown and li id, compare, hide where not true
 
-var userInput = document.getElementById("#resource-filter");
-userInput.addEventListener("change", function () {
-  var filterResources = RESOURCES.filter(function (card) {
-    return card().indexOf(userInput.value) !== -1;
-  });
-  render_(filterResources);
+var userSelect = document.querySelector("#resource-filter");
+userSelect.addEventListener("change", function () {
+  console.log(userSelect.value);
+  var element = [document.querySelectorAll("li#cardjs")];
+
+  console.log(element);
+
+  //somewhat hot mess below
+
+  var i;
+
+  for (i = 0; i < element.length; i++) {
+    console.log(element[i]);
+
+    if (element[i].classList.contains(userSelect.value)) {
+      element[i].style.display = "";
+    } else {
+      element[i].style.display = "none";
+    }
+  }
 });
+
+//abandoned below
+
+// if (document.getElementsByClassName("lessonplans") == userSelect.value) {
+//   element.classList.add("selectstyle");
+// } else if (
+//   document.getElementsByClassName("professionaldev") == userSelect.value
+// ) {
+//   element.classList.add("selectstyle");
+// } else if (
+//   document.getElementsByClassName("nationalorgs") == userSelect.value
+// ) {
+//   element.classList.add("selectstyle");
+// } else if (
+//   document.getElementsByClassName("regionalorgs") == userSelect.value
+// ) {
+//   element.classList.add("selectstyle");
+// } else {
+//   //show all?
+// }
+// });
